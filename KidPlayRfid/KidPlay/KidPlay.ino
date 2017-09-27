f3b7cf2a97ba48c8172d5d304d6bf0bc76395cfa
kleinsimon <sklein8@googlemail.com>
2017-09-27 06:59:19 +0200

    new with rfid tag

--- KidPlayRfid/KidPlay/KidPlay.ino
+++ KidPlayRfid/KidPlay/KidPlay.ino
@@ -3,6 +3,12 @@
  Created:	09.09.2017 22:19:30
  Author:	Simon Klein (simon.klein@outlook.com)
 */
+#include <require_cpp11.h>
+#include <MFRC522Hack.h>
+#include <MFRC522Extended.h>
+#include <MFRC522Debug.h>
+#include <MFRC522.h>
+#include <deprecated.h>
 #include <EEPROM.h>
 #include <SPI.h>
 #include <Adafruit_VS1053.h>
@@ -16,17 +22,20 @@
 #define SHIELD_DCS    6      // VS1053 Data/command select pin (output)
 #define CARDCS 4     // Card chip select pin
 #define DREQ 3       // VS1053 Data request, ideally an Interrupt pin
-#define KEEPALIVE 5 //Pin for keep-alive-resistor (~ 150 OHM)
 
-// Pins for the Folder ID Switches. Order is lowest bit to highest bit. The more pins, the more folders can be addressed.
-const uint8_t fidBtnPins[6] = {8,9,10,11,12,13};
+// RFID 522 Pins:
+#define RFIS_RST_PIN -1
+#define RFIS_SS_PIN 10
 
-// Pins for Buttons
-#define btnPause 3
-#define btnVolUp 1
-#define btnVolDown 2
-#define btnNext 5
-#define btnPrev 4
+// Pins for Buttons: VolUp, VolDown, Pause, Prev, Next
+#define nBtnPins 5
+uint8_t btnPins[nBtnPins] = {12,13,11,8,9};
+
+#define btnVolUp 0
+#define btnVolDown 1
+#define btnPause 2
+#define btnPrev 3
+#define btnNext 4
 
 // Eeprom Adresses
 #define memAdrVol 11	//one byte, last volume
@@ -45,11 +54,12 @@ const uint8_t fidBtnPins[6] = {8,9,10,11,12,13};
 // Delay before switching to new folder in loops
 #define switchDelay 100
 
-// Keep-Alive durations
-#define loopKeepAlive 200
-#define keepAliveDuration 50
-
 Adafruit_VS1053_FilePlayer musicPlayer = Adafruit_VS1053_FilePlayer(SHIELD_RESET, SHIELD_CS, SHIELD_DCS, DREQ, CARDCS);
+MFRC522 rfid(RFIS_SS_PIN, RFIS_RST_PIN);
+MFRC522::MIFARE_Key key;
+
+byte nuidPICC[4];
+
 
 // Stacks for File search
 char curFile[13];
@@ -57,20 +67,15 @@ char nearest[13];
 char tmpFile[13];
 
 // Stacks for Button state and Event state
-bool btnState[8];
-bool btnTemp[8];
-bool btnConsumed[8];
+bool btnState[nBtnPins];
+bool btnTemp[nBtnPins];
+bool btnConsumed[nBtnPins];
 
 // State of the player
 bool stopped = false;
 bool paused = false;
 
 int delayTimer = 0;
-
-#if KEEPALIVE > 0
-	int keepAliveTimer = 0;
-	int keepAliveCounter = 0;
-#endif
 
 uint8_t volume = 40;
 uint8_t curFolderNumber = 0;
@@ -81,6 +86,9 @@ char curFolder[3];
 void setup() {
 	Serial.begin(9600);
 	Serial.println("Initializing musicplayer...");
+	SPI.begin();
+	rfid.PCD_Init();
+	rfid.PCD_DumpVersionToSerial();
 
 	if (!musicPlayer.begin()) { // initialise the music player
 		Serial.println(F("Couldn't find VS1053, do you have the right pins defined?"));
@@ -95,6 +103,10 @@ void setup() {
 	// Initialization
 	initButtons();
 	musicPlayer.useInterrupt(VS1053_FILEPLAYER_PIN_INT);
+
+	for (byte i = 0; i < 6; i++) {
+		key.keyByte[i] = 0xFF;
+	}
 	
 	// Read last state from eeprom
 	lastFolderNumber = EEPROM.read(memLastDir);
@@ -123,11 +135,6 @@ void setup() {
 		musicPlayer.currentTrack.seek(lastPos);
 		musicPlayer.pausePlaying(false);
 	}
-
-#if KEEPALIVE > 0
-	pinMode(KEEPALIVE, OUTPUT);
-	toggleKeepAlive(true);
-#endif
 }
 
 // Main loop
@@ -139,6 +146,8 @@ void loop() {
 	}
 	else if (checkEvent(btnNext)) {
 		musicPlayer.stopPlaying();
+		findNextFile();
+		playFile();
 	}
 	else if (checkEvent(btnPrev)) {
 		musicPlayer.stopPlaying();
@@ -152,7 +161,11 @@ void loop() {
 		raiseVolume(volChange);
 	}
 
-	int fidState = readFidState();
+	if (rfid.PICC_IsNewCardPresent()) {
+		readRfid();
+	}
+
+	int fidState = 0;
 	if (fidState != curFolderNumber) {
 		if (delayTimer > switchDelay) {
 			musicPlayer.stopPlaying();
@@ -178,22 +191,50 @@ void loop() {
 		EEPROM_writeAnything(memLastPos, musicPlayer.currentTrack.position());
 	}
 
-#if KEEPALIVE < 0
-	if (keepAliveTimer > loopKeepAlive) {
-		if (keepAliveCounter == 0) {
-			toggleKeepAlive(true);
-		}
-		keepAliveCounter++;
-		if (keepAliveCounter > keepAliveDuration) {
-			keepAliveCounter = 0;
-			keepAliveTimer = 0;
-			toggleKeepAlive(false);
-		}
-	}
-	keepAliveTimer++;
-#endif
-
 	delay(loopSleep);
+}
+
+void readRfid() {
+	if (!rfid.PICC_ReadCardSerial())
+		return;
+
+	MFRC522::PICC_Type piccType = rfid.PICC_GetType(rfid.uid.sak);
+
+	// Check is the PICC of Classic MIFARE type
+	if (piccType != MFRC522::PICC_TYPE_MIFARE_MINI &&
+		piccType != MFRC522::PICC_TYPE_MIFARE_1K &&
+		piccType != MFRC522::PICC_TYPE_MIFARE_4K) {
+		Serial.println(F("Your tag is not of type MIFARE Classic."));
+		return;
+	}
+
+	if (rfid.uid.uidByte[0] == nuidPICC[0] ||
+		rfid.uid.uidByte[1] == nuidPICC[1] ||
+		rfid.uid.uidByte[2] == nuidPICC[2] ||
+		rfid.uid.uidByte[3] == nuidPICC[3]) {
+		return;
+	}
+
+	Serial.print(F("PICC type: "));
+	Serial.println(rfid.PICC_GetTypeName(piccType));
+
+	Serial.println(F("A new card has been detected."));
+
+	// Store NUID into nuidPICC array
+	for (byte i = 0; i < 4; i++) {
+		nuidPICC[i] = rfid.uid.uidByte[i];
+	}
+
+	Serial.println(F("The NUID tag is:"));
+	Serial.print(F("In hex: "));
+	printHex(rfid.uid.uidByte, rfid.uid.size);
+	Serial.println();
+	Serial.print(F("In dec: "));
+	printDec(rfid.uid.uidByte, rfid.uid.size);
+	Serial.println();
+
+	rfid.PICC_HaltA();
+	rfid.PCD_StopCrypto1();
 }
 
 void initDir() {
@@ -201,7 +242,7 @@ void initDir() {
 	strcpy(curFile, "");
 
 	// Read Folder ID from GPIOs
-	curFolderNumber = readFidState();
+	curFolderNumber = 0;
 	sprintf(curFolder, "%d", curFolderNumber);
 
 	//printDirectory(SD.open(curFolder),0);
@@ -214,14 +255,6 @@ void initDir() {
 	// Store current folder number in eeprom
 	EEPROM.write(memLastDir, curFolderNumber);
 }
-
-#if KEEPALIVE > 0
-void toggleKeepAlive(bool kaState) {
-	digitalWrite(KEEPALIVE, kaState);
-	Serial.print("Keepalive: ");
-	Serial.println(kaState);
-}
-#endif
 
 //Check for an event and consume it
 bool checkEvent(uint8_t pin) {
@@ -272,41 +305,25 @@ void raiseVolume(uint8_t dif) {
 	setVolume(volume + dif);
 }
 
-// Initialize all Buttons on the shield and the FID-Buttons on arduino
+// Initialize all Buttons on arduino
 void initButtons() {
-	for (uint8_t i = 0; i < 8; i++)
-		musicPlayer.GPIO_pinMode(i, INPUT);
-	for (uint8_t i = 0; i < sizeof(fidBtnPins); i++)
-		pinMode(fidBtnPins[i], INPUT_PULLUP);
+	for (uint8_t i = 0; i < nBtnPins; i++)
+		pinMode(btnPins[i], INPUT_PULLUP);
 }
 
 // Read the shield buttons state and reset event state
 void readButtons() {
-	for (uint8_t i = 0; i < 8; i++) {
+	for (uint8_t i = 0; i < nBtnPins; i++) {
 		btnTemp[i] = btnState[i];
-		btnState[i] = musicPlayer.GPIO_digitalRead(i);
+		btnState[i] = !digitalRead(btnPins[i]);
 		if (btnTemp[i] && !btnState[i] && btnConsumed[i]) btnConsumed[i] = false; //Reset if now not pressed
 	}
-}
-
-// Read the fid state to a number. Low means pressed (internal pullup)
-int readFidState() {
-	bool fidTmp[6];
-	int res = 0;
-	//Serial.print("FID State: ");
-	for (uint8_t i = 0; i < sizeof(fidBtnPins); i++) {
-		bitWrite(res, i, digitalRead(fidBtnPins[i]));
-		//Serial.print(!digitalRead(fidBtnPins[i]));
-
-	}
-	//Serial.println(" ");
-	return res;
 }
 
 // Print the FID-State to Serial
 void plotBtnState() {
 	Serial.print("Buttons: [");
-	for (uint8_t i = 0; i < 8; i++)
+	for (uint8_t i = 0; i < nBtnPins; i++)
 		Serial.print(btnState[i]);
 	Serial.println("]");
 }
@@ -455,5 +472,25 @@ void printDirectory(File dir, int numTabs) {
 			Serial.println(entry.size(), DEC);
 		}
 		entry.close();
+	}
+}
+
+/**
+* Helper routine to dump a byte array as hex values to Serial.
+*/
+void printHex(byte *buffer, byte bufferSize) {
+	for (byte i = 0; i < bufferSize; i++) {
+		Serial.print(buffer[i] < 0x10 ? " 0" : " ");
+		Serial.print(buffer[i], HEX);
+	}
+}
+
+/**
+* Helper routine to dump a byte array as dec values to Serial.
+*/
+void printDec(byte *buffer, byte bufferSize) {
+	for (byte i = 0; i < bufferSize; i++) {
+		Serial.print(buffer[i] < 0x10 ? " 0" : " ");
+		Serial.print(buffer[i], DEC);
 	}
 }
